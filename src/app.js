81fcfc4960e60f0273ace89d0ca514e5b9c69d80
Tim Paulaskas <drookue@gmail.com>
2020-06-03 15:33:00 -0700

    Init commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.json())
+
+app.use(express.static(publicDirectoryPath))
+
+module.exports = app

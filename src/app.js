347bb23060865dd84efe6fd3c440a27b4de8d231
Tim Paulaskas <drookue@gmail.com>
2020-06-03 15:45:09 -0700

    Added path reference

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')

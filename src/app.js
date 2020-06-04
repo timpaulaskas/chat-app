7841418c58a722a84c95000a8ff04777c85ddf37
Tim Paulaskas <drookue@gmail.com>
2020-06-04 09:41:15 -0700

    Added msg when a user joins or leaves

--- src/app.js
+++ src/app.js
@@ -20,8 +20,14 @@ io.on('connection', (socket) => {
 
     socket.emit('message', 'Welcome!')
 
+    socket.broadcast.emit('message', 'A new user has joined')
     socket.on('message', (message) => {
         io.emit('message', message)
     })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
 })
+
 module.exports = server

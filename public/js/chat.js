6e3c7301b25e509ddd5d99cf5479d9d29a7628dc
Tim Paulaskas <drookue@gmail.com>
2020-06-04 11:10:35 -0700

    Added mustache to render dynamic templates

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,35 +2,51 @@ const socket = io()
 
 const messageForm = document.querySelector('#message-form')
 const chatMessage = document.querySelector('#message')
-const chatArea = document.querySelector('#chatArea')
-const sendLocation = document.querySelector('#send-location')
+const chatButton = document.querySelector('#sendMessage')
+const sendLocationButton = document.querySelector('#send-location')
+const messages = document.querySelector('#messages')
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
-    chatArea.innerHTML = chatArea.innerHTML + '<span class="timestamp">' + new Date().toLocaleString() + '</span><span class="message">' + message + '</span><br/>'
+    const html = Mustache.render(messageTemplate, {
+        time: new Date().toLocaleString(),
+        message
+    })
+    messages.insertAdjacentHTML('afterbegin', html)
 })
 
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
     const message = chatMessage.value
-    socket.emit('message', message, (error) => {
-        if (error) {
-            return alert(error)
-        }
-        console.log('Message delivered')
-        chatMessage.value = ''
-    })
+    if (message !== '') {
+        chatButton.setAttribute('disabled', 'disabled')
+        socket.emit('message', message, (error) => {
+            chatButton.removeAttribute('disabled')
+            if (error) {
+                return alert(error)
+            }
+            console.log('Message delivered')
+            chatMessage.value = ''
+            chatMessage.focus()
+        })
+    }
+
 })
 
-sendLocation.addEventListener('click', () => {
+sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
 
+    sendLocationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, (error) => {
+            sendLocationButton.removeAttribute('disabled')
             if (error) {
                 return alert(error)
             }

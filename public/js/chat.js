8334a7efe6921b8bb6a3e34a49a3243a219206c5
Tim Paulaskas <drookue@gmail.com>
2020-06-04 09:57:10 -0700

    Added geolocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,6 +3,7 @@ const socket = io()
 const messageForm = document.querySelector('#message-form')
 const chatMessage = document.querySelector('#message')
 const chatArea = document.querySelector('#chatArea')
+const sendLocation = document.querySelector('#send-location')
 
 socket.on('message', (message) => {
     chatArea.innerHTML = chatArea.innerHTML + '<span class="timestamp">' + new Date().toLocaleString() + '</span><span class="message">' + message + '</span><br/>'
@@ -13,4 +14,16 @@ messageForm.addEventListener('submit', (e) => {
     const message = chatMessage.value
     chatMessage.value = ''
     socket.emit('message', message)
+})
+
+sendLocation.addEventListener('click', () => {
+    if (sendLocation.checked) {
+        if (!navigator.geolocation) {
+            return alert('Geolocation is not supported by your browser')
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position)
+        })
+    }
 })
